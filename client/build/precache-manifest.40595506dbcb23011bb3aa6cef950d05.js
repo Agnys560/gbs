faa220a805b1422e9da14812829feeba1ffcd88f
Jonathan Modlin <jonathanmodlin1@gmail.com>
2020-07-07 10:12:43 -0400

    heroku

---
+++ client/build/precache-manifest.40595506dbcb23011bb3aa6cef950d05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "438b976a00261fb0a62d",
+    "url": "/static/css/main.23abdc19.chunk.css"
+  },
+  {
+    "revision": "438b976a00261fb0a62d",
+    "url": "/static/js/main.ed3075b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "db05ec5d8dabfcdc0e8f",
+    "url": "/static/js/2.94f5cbce.chunk.js"
+  },
+  {
+    "revision": "a94ce74134950bd24ca6a56b59c64919",
+    "url": "/static/media/books.a94ce741.jpg"
+  },
+  {
+    "revision": "11ab095c51be440528a4e74012e76735",
+    "url": "/index.html"
+  }
+];
